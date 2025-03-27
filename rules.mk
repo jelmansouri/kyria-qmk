65670b122d03783b55195f2d29a3756fcef5a717
Jalal El Mansouri <jalal.elmansouri@gmail.com>
2025-03-27 09:31:40 -0400

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -3,7 +3,3 @@ SRC +=  oled.c
 
 # converting to RP2040ce/liatris
 CONVERT_TO=liatris
-OLED_ENABLE = yes
-ENCODER_ENABLE = no       # Disable the use of one or more encoders
-RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
